4f953ea25df99ab47f787eb94608a5371d092f95
Juan Nogueira <juannogueiraxavier@gmail.com>
2023-12-23 21:53:46 -0300

    Lendo arquivos com file e Scanner

---
+++ Trabalhando com Arquivos 1/src/aplication/Program.java
@@ -0,0 +1,29 @@
+package aplication;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		File file = new File("C:\\Users\\jxavier\\eclipse-workspace\\Trabalhando com Arquivos 1\\in.txt");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		finally{
+			if(sc != null) {
+				sc.close();
+			}
+		}
+	}
+
+}
